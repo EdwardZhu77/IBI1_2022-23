f983c31c6acc6e70e9659581cf70ec1f62837cc7
EdwardZhu77 <127014656+EdwardZhu77@users.noreply.github.com>
2023-03-21 23:19:31 +0800

    Update variable.py

--- Practical5/variable.py
+++ Practical5/variable.py
@@ -1,1 +1,12 @@
+a = -3.19
+b = -118.24
+c = 116.39
+d = a-b
+e = c-a
+if d < e: print("e is greater than d and Rob travel further to Los Angeles")
+else: print("d is greater than e and Rob travel further to Haining")
+X = True
+Y = False
 
+W = X and Y  # W is False
+Z = X or Y   # Z is True
