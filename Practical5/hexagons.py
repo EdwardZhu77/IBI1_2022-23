0d2e8542eee7164f53aff6739f0293aafe650cab
EdwardZhu77 <127014656+EdwardZhu77@users.noreply.github.com>
2023-03-23 18:33:41 +0800

    Update hexagons.py

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,15 +1,17 @@
+# we need several "n" to get several "h"
+# so I go through to whole progress for five times
 n=1
 h = 2*n*(2*n-1)/2
-print(h)
+print(h) # "h"corresponding with "n=1"
 n=2
 h = 2*n*(2*n-1)/2
-print(h)
+print(h) # "h"corresponding with "n=2"
 n=3
 h = 2*n*(2*n-1)/2
-print(h)
+print(h) # "h"corresponding with "n=3"
 n=4
 h = 2*n*(2*n-1)/2
-print(h)
+print(h) # "h"corresponding with "n=4"
 n=5
 h = 2*n*(2*n-1)/2
-print(h)
+print(h) # "h"corresponding with "n=5"
