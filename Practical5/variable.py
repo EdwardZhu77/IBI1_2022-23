5d0c18e3ac4909c37a3e7d3497e688d6acc953e6
EdwardZhu77 <127014656+EdwardZhu77@users.noreply.github.com>
2023-03-23 18:27:57 +0800

    Update variable.py

--- Practical5/variable.py
+++ Practical5/variable.py
@@ -1,12 +1,12 @@
 a = -3.19
 b = -118.24
 c = 116.39
-d = a-b
-e = c-a
+d = a-b # compare positive number needed for comparsion
+e = c-a # compare positive number needed for comparsion
 if d < e: print("e is greater than d and Rob travel further to Los Angeles")
 else: print("d is greater than e and Rob travel further to Haining")
 X = True
 Y = False
-
 W = X and Y  # W is False
 Z = X or Y   # Z is True
+print(W and Z) # W and Z is false
