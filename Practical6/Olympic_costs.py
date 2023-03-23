e8ffa2b5ae823040df074896524b4e5813cb9400
EdwardZhu77 <127014656+EdwardZhu77@users.noreply.github.com>
2023-03-23 21:20:48 +0800

    Update Olympic_costs.py

--- Practical6/Olympic_costs.py
+++ Practical6/Olympic_costs.py
@@ -11,3 +11,27 @@ for cost_string in cost_strings:
     # Append the cost to the list.
     costs.append(cost)
 print(costs)
+
+ import matplotlib.pyplot as plt # open pyblot
+>>>
+>>> data = [("Los Angeles 1984", 1),
+...         ("Seoul 1988", 8),
+...         ("Barcelona 1992", 15),
+...         ("Atlanta 1996", 7),
+...         ("Sydney 2000", 5),
+...         ("Athens 2004", 14),
+...         ("Beijing 2008", 43),
+...         ("London 2012", 40)]
+>>> data.sort(key=lambda x: x[1])
+>>> x_labels = [x[0] for x in data]
+>>> y_values = [x[1] for x in data] # similar to dictionary
+>>>
+>>> plt.bar(x_labels, y_values)
+<BarContainer object of 8 artists>
+>>> plt.xlabel("City") # x axi
+Text(0.5, 0, 'City')
+>>> plt.ylabel("Cost (billion dollars)") # y axi
+Text(0, 0.5, 'Cost (billion dollars)')
+>>> plt.title("Cost of Hosting the Summer Olympic Games")
+Text(0.5, 1.0, 'Cost of Hosting the Summer Olympic Games')
+>>> plt.show() # show the chart
