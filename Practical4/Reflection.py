5b09607d35fc0ac7c47b98ebb09afd797277f5ca
EdwardZhu77 <127014656+EdwardZhu77@users.noreply.github.com>
2023-05-22 18:43:27 +0800

    Final Check and modify

---
+++ Practical4/Reflection.py
@@ -0,0 +1,7 @@
+In practical 4 I learned how to use git command to set up a working branch and make changes to the branch content on the local computer. It is a little bit difficult for beginer like me.
+
+In the process of git practice, I think it is difficult to make clear that where you actually are.
+
+In the process of building web pages, I think the more difficult is how to change the style.
+
+In the whole IBI, I found it is a little bit difficult for me :( 
