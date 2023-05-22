7a194d0ce9d7fedb48f5a59c958d4467dd1033ab
EdwardZhu77 <127014656+EdwardZhu77@users.noreply.github.com>
2023-05-22 19:09:11 +0800

    Final Check

--- Practical5/variable.py
+++ Practical5/variable.py
@@ -1,12 +1,22 @@
+# Store the longitude of Edinburgh as a
 a = -3.19
+# Store the longitude of Los Angeles as b
 b = -118.24
+# Store the longitude of Haining as c
 c = 116.39
-d = a-b # compare positive number needed for comparsion
-e = c-a # compare positive number needed for comparsion
+d = abs(a-b) # Calculate the longitude distance  as the difference between a and b. Store this value in a variable called d, abs for absolute value.
+e = abs(c-a) # Calculate the longitude distance  as the difference between and a c. Store this value in a variable called e, abs for absolute value.
 if d < e: print("e is greater than d and Rob travel further to Los Angeles")
 else: print("d is greater than e and Rob travel further to Haining")
+# Create variable X to store Boolean variables True
 X = True
+# Create variable Y to store Boolean variables False
 Y = False
-W = X and Y  # W is False
-Z = X or Y   # Z is True
-print(W and Z) # W and Z is false
+# Creat W to encode the Boolean variables “X an Y"
+W = X and Y
+# Creat Z to encode the Boolean variables “X or Y"
+Z = X or Y
+
+# Print the value of W and Z separately
+print("The value of W is " , W) # Value of W
+print("The value of Z is " , Z) # Value of Z
