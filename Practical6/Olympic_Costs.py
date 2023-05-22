2d7ffa4e95727be9fef879c4a77a2273b966c493
EdwardZhu77 <127014656+EdwardZhu77@users.noreply.github.com>
2023-05-22 22:55:06 +0800

    Final Check and modify

--- Practical6/Olympic_Costs.py
+++ Practical6/Olympic_Costs.py
@@ -2,13 +2,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Olympic Games and corresponding cost
+Olympic_games = ['Los Angeles 1984', 'Seoul 1988', 'Barcelona 1992', 'Atlanta 1996', 'Sydney 2000', 'Athens 2003', 'Beijing 2008', 'London 2012']
+costs = [1, 8, 15, 7, 5, 14, 43, 40] 
+data = list(zip(Olympic_games, costs))
+
 # Define the number of data points
 N = 8
-
-# Olympic Games and corresponding cost
-olympic_games = ['Los Angeles 1984', 'Seoul 1988', 'Barcelona 1992', 'Atlanta 1996', 'Sydney 2000', 'Athens 2003', 'Beijing 2008', 'London 2012']
-costs = [1, 8, 15, 7, 5, 14, 43, 40]
-data = list(zip(olympic_games, costs))
 
 # Sort the data based on cost
 sorted_data = sorted(data, key=lambda x: x[1])
@@ -19,18 +19,18 @@ sorted_costs = [cost for game, cost in sorted_data]
 
 # Set up the plot
 ind = np.arange(N)
-width = 0.35
+width = 0.5
 pl = plt.bar(ind, sorted_costs, width)
 
 # Set the title and axis labels for the plot
-plt.ylabel('Costs (in $ billions)')
+plt.ylabel('Costs (in billion dollars)')
 plt.title('Olympic Costs')
 plt.xticks(ind, sorted_olympic_games, rotation=45)
 
 # Set the length and scale of the vertical axis
 plt.yticks(np.arange(0, 51, 10))
 
-# Display the plot
+# Show the plot
 plt.show()
 
 # Print the costs
