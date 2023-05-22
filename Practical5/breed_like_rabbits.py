0d8cdba46e541ed809886ff094d8f362b0e6a796
EdwardZhu77 <127014656+EdwardZhu77@users.noreply.github.com>
2023-05-22 18:55:17 +0800

    Final Check

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,8 +1,15 @@
-# We want no mor than 100 rabbits
+# We want no more than 100 rabbits
 # We could know the number of rabbit is in exponential growth
-n = 1   # number of generation
-m = 2**n   # C
-while m < 100:
-    n = n + 1 # next generation
-    m = 2**n # number of generation
-print(n-1)   # number of generation
+# Initialize the number of rabbits to 2
+# Initialize the generation counter to 1
+generation = 1   # Number of generation
+number_of_rabbits = 2 # Number of rabitts
+# Use a while loop to simulate rabbit breeding
+while number_of_rabbits <= 100:
+    # Next generation
+	number_of_rabbits *= 2
+    # Next generation
+	generation += 1
+   
+# Print a message stating the generation at which the total number of rabbits exceeds 100
+print ("At generation", generation ,"over 100 rabits have been born")
